3b458c079c65e8c2e7e143e169ef3740193e1170
msaltzman5 <89426885+msaltzman5@users.noreply.github.com>
2024-02-17 12:15:57 -0600

    Create main.py

---
+++ Logic/main.py
@@ -0,0 +1,34 @@
+from enum import Enum
+
+class Card():
+  Suit,
+  Rank
+
+class Suit(Enum):
+    HEARTS = "H"
+    DIAMONDS = "D"
+    CLUBS = "C"
+    SPADES = "S"
+
+class Rank(Enum):
+    ACE = 1
+    TWO = 2
+    THREE = 3
+    FOUR = 4
+    FIVE = 5
+    SIX = 6
+    SEVEN = 7
+    EIGHT = 8
+    NINE = 9
+    TEN = 10
+    JACK = 11
+    QUEEN = 12
+    KING = 13
+
+card = input()
+
+suit = card.rstrip(card[-1])
+rank = card
+
+def main():
+  print("hello")
