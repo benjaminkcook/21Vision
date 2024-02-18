d919c9e338325be469e6a43ab5edaa89b49a4aa9
viet-ninh <78924733+viet-ninh@users.noreply.github.com>
2024-02-18 05:10:51 -0600

    Adding drawing ability

--- object_detection/screenshot_capture.py
+++ object_detection/screenshot_capture.py
@@ -17,7 +17,6 @@ while True:
     if not ret:
         print("failed to grab frame")
         break
-    cv2.imshow("21 Vision", frame)
 
     k = cv2.waitKey(1)
     if k%256 == 27:
@@ -30,11 +29,30 @@ while True:
         cv2.imwrite(img_path, frame)
         outs = model.predict(img_path)
         img_counter += 1
+        
+        image = cv2.imread(img_path)
 
         for result in outs:
             if len(result.boxes.cls) > 0:
                 detect_objects = result.boxes.cls.numpy()
-                names = [result.names[key] for key in detect_objects]
-                cards = set(names)
+                object_conf = result.boxes.conf.numpy()
+                for i in range(len(detect_objects)):
+                    box = result.boxes[i]
+                    confidence = round(object_conf[i], 2)
+                    # Contains x, y ,w, h positions to draw stuff
+                    pos_list = box.xywh.numpy()
+                    x = int(pos_list[0, 0])
+                    y = int(pos_list[0, 1])
+                    w = int(pos_list[0, 2])
+                    h = int(pos_list[0, 3])
+                    color = (0, 255, 0)
+                    cv2.rectangle(image, (x, y), (x + w, y + h), color, 2)
+                    label = f"Class {result.names[detect_objects[i]]}, Confidence: {confidence}"
+                    cv2.putText(image, label, (x, y - 10), cv2.FONT_HERSHEY_SIMPLEX, 0.5, color, 2)
+                    cv2.imwrite(img_path, image)
 
+                    names = [result.names[key] for key in detect_objects]
+                    cards = set(names)
+
+    cv2.imshow("21 Vision", frame)
 camera.release()
