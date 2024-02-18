74da107906388122dc734bf5e3d5093b3a0865a3
viet-ninh <78924733+viet-ninh@users.noreply.github.com>
2024-02-18 05:48:00 -0600

    Drawing boxes in real time capture

--- object_detection/real_time_capture.py
+++ object_detection/real_time_capture.py
@@ -25,9 +25,23 @@ while True:
     for result in results:
         if len(result.boxes.cls) > 0:
             detect_objects = result.boxes.cls.numpy()
-            names = [result.names[key] for key in detect_objects]
-            cards = set(names)
+            object_conf = result.boxes.conf.numpy()
+            for i in range(len(detect_objects)):
+                box = result.boxes[i]
+                confidence = round(object_conf[i], 2)
+                # Contains x, y ,w, h positions to draw stuff
+                pos_list = box.xywh.numpy()
+                x = int(pos_list[0, 0])
+                y = int(pos_list[0, 1])
+                w = int(pos_list[0, 2])
+                h = int(pos_list[0, 3])
+                color = (0, 255, 0)
+                cv2.rectangle(frame, (x, y), (x + w, y + h), color, 2)
+                label = f"Class {result.names[detect_objects[i]]}, Confidence: {confidence}"
+                cv2.putText(frame, label, (x, y - 10), cv2.FONT_HERSHEY_SIMPLEX, 0.5, color, 2)
 
+                names = [result.names[key] for key in detect_objects]
+                cards = set(names)
     cv2.imshow("21 Vision", frame)
 
     if cv2.waitKey(1) & 0xFF == ord("q"):
